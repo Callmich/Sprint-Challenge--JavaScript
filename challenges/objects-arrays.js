047b74b0ee71541e203feb9fb500a42623fcafcd
Michael Callahan <CallahanMichaelE@gmail.com>
2020-01-31 11:43:01 -0700

    request 3 solved

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -101,6 +101,13 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach(function(item){
+  if (item.university.includes(`Uni`)){
+  unisWithUni.push(item)
+  }
+})
+
+
 console.log(unisWithUni);
 
 
